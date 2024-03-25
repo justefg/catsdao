5e8996032042d6c5ff5c72d0b93f4a6d22eb348d
Emil Guseynov <theluckyemil@gmail.com>
2024-03-25 14:58:56 +0400

    add support for voting token

--- contracts/test/ERC20VotesMock.sol
+++ contracts/test/ERC20VotesMock.sol
@@ -6,4 +6,16 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract ERC20VotesMock is ERC20Votes {
   constructor() ERC20("Vote Token", "VTT") EIP712("", "") {}
+
+  function mint(address to, uint amount) external {
+    _mint(to, amount);
+  }
+
+  function clock() public view virtual override(Votes) returns (uint48) {
+    return uint48(block.timestamp);
+  }
+
+  function CLOCK_MODE() public view virtual override(Votes) returns (string memory) {
+    return "mode=timestamp";
+  }
 }
